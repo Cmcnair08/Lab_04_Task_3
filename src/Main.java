b552dc474df73256a3d69dddbd64a835eaf9d291
ChristianMcNair <mcnairce@mail.uc.edu>
2025-02-13 11:54:35 -0500

    my final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    double balance = 5000;
+    final double INTEREST_RATE =.17;
+    double interestCalc = 0;
+    interestCalc = balance * INTEREST_RATE;
+    balance = balance + interestCalc;
+    System.out.println("The interest after one month is: " + interestCalc);
+    interestCalc = balance * INTEREST_RATE;
+        System.out.println("The interest after the second month is: " + interestCalc);
     }
 }
